3555957c6fcadca348d123f30ebbaa1768f405b8
nablusi <zaidsaeh3@gmail.com>
2025-06-11 15:22:52 +0300

    add routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 require('dotenv').config()
 const express = require('express')
-
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body
+  const mockUser = {
+    username: 'admin',
+    password: '123456' 
+  }
+
+  if (username === mockUser.username && password === mockUser.password) {
+    res.status(200).json({ message: 'Login successful' })
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' })
+  }
 })
 
 app.listen(port, () => {
